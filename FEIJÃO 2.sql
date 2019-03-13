7dba231382bc8a30bfc86753e8c4f2fd09e0a4d7
bernizv <47605723+bernizv@users.noreply.github.com>
2019-03-13 18:17:07 -0300

    SCRIPT
    DE CONSULTA E INSERÇÃO DE DADOS.
    
    FEIJÃO 1 É APENAS COM A CRIAÇÃO E INSERÇÃO DE DADOS NA TABELA E NO FEIJÃO 2 É COM UM COMANDO PEDINDO PARA MOSTRAR AS TEMPERATURAS >= (MAIOR OU IGUAL) A 30

---
+++ FEIJÃO 2.sql
@@ -0,0 +1,17 @@
+create table sensor_feijao2 (
+    idFeijao int primary key identity (100,10),
+    nomeFeijao varchar(30),
+    safras varchar(30),
+    regiao varchar(30),
+    temperatura int,
+    umidade int
+    );
+    select * from sensor_feijao2;
+
+insert into sensor_feijao2
+            values  ('Carioca', 'Janeiro-Julho', 'Sul', 27, 98),
+                    ('Preto', 'Julho-Outubro', 'Nordeste', 33, 64),
+                    ('Fradinho', 'Abril-Julho', 'Sudeste', 19, 42),
+                    ('Verde','Janeiro-Abril', 'Norte', 50, 100); 
+
+select * from sensor_feijao where temperatura >= 30;
